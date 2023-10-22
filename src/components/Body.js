65fdf1b0930531b3e986933fce4236b7549b0c04
Aashish Kumar Jha <20bcs3872@cuchd.in>
2023-10-22 22:58:04 +0530

    started making hamburger menu

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const Body = () => {
   return (
-    <div>Body</div>
-  )
-}
+    <div className="grid grid-flow-col ">
+      <div className="flex grid-cols-2 border-2 shadow-lg w-44 ">
+        <h1>Hamburger Menu</h1>
+      </div>
+      <div className="flex grid-cols-10">
+        <h1>Video Container</h1>
+      </div>
+    </div>
+  );
+};
 
 export default Body;
