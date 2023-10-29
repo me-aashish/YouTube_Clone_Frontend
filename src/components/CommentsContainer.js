81e4359c472255c1c2ca34837993ba06e371f080
Aashish Kumar Jha <20bcs3872@cuchd.in>
2023-10-29 18:48:10 +0530

    added fucntionality to add a new comment

--- src/components/CommentsContainer.js
+++ src/components/CommentsContainer.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import UserIconLink from '../constants/userIconLink';
+import sendMessageIconLink from '../constants/sendMessageIconLink';
 
 const commentsData = [
   {
@@ -86,12 +87,67 @@ const CommentsList = ({ comments }) => {
 }
 
 const CommentsContainer = () => {
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleAddComment = (name, message) => {
+    const newComment = 
+      {
+        name: name,
+        text: message,
+        replies: []
+      }
+    
+    console.log(newComment);
+    commentsData.unshift(newComment);
+
+  }
+
   return (
     <div className="p-3 m-7 w-[800px]">
-        <h1 className="text-2xl font-bold">Comments:</h1>
-        <CommentsList comments={commentsData} />
+      <h1 className="text-2xl font-bold">Comments:</h1>
+      <div className="flex h-[50px]">
+        <span>
+          <input
+            type="text"
+            placeholder="name"
+            className="mt-2 ml-3 border p-2 border-black bg-white-50 shadow-xl w-[100px]  rounded-l-lg h-[41px]"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </span>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            handleAddComment(name, message);
+            setName("");
+            setMessage("");
+          }}
+        >
+          <input
+            type="text"
+            placeholder="comment"
+            className="mt-2 border p-2 border-black bg-white-50 shadow-xl w-[600px] h-[41px]"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+        </form>
+        <span>
+          <img
+            src={sendMessageIconLink}
+            alt="send"
+            className="w-[60px] mt-2 border border-black   rounded-r-lg h-[41px] cursor-pointer"
+            onClick={() => {
+              handleAddComment(name, message);
+              setName("");
+              setMessage("");
+            }}
+          />
+        </span>
+      </div>
+      <CommentsList comments={commentsData} />
     </div>
-  )
+  );
 }
 
 export default CommentsContainer
